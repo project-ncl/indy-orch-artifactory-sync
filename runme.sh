90329dd72309d625a15732da6d393afb9377e2b5
Dustin Kut Moy Cheung <dcheung@redhat.com>
2026-07-28 15:19:15 -0400

    Some cleanups

--- runme.sh
+++ runme.sh
@@ -4,5 +4,5 @@ source env.sh
 psql -h ${PGSERVER} -d ${PGDB} ${PGUSER} \
      -c "\copy (select tr.repositorypath, ar.deploypath from artifact ar join targetrepository tr on ar.targetrepository_id = tr.id where ar.modificationtime >= '${FROM}' and ar.modificationtime < '${TO}' and tr.repositorypath like '%/hosted/%') to '/tmp/artifacts-${FROM}--${TO}.csv' WITH (format csv)"
 
-python download.py --csv /tmp/artifacts-${FROM}--${TO}.csv --output-dir /tmp/artifacts-${FROM}--${TO}
-python upload.py --metadata /tmp/artifacts-${FROM}--${TO}/metadata.json --dry-run
+python download.py --csv /tmp/artifacts-${FROM}--${TO}.csv --output-dir /mnt/db-migration/indy-data/artifacts-${FROM}--${TO}
+# python upload.py --metadata /tmp/artifacts-${FROM}--${TO}/metadata.json --dry-run
